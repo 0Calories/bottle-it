44b730073952b56fc4141347d4186b616d22aa1c
0Calories <cppanand@gmail.com>
2018-06-19 00:12:06 -0400

    Update server.js to use request params for creating messages for POST route

--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 
 let app = require('express')();
+const bodyParser = require('body-parser');
 const {MONGODB_URI, PORT} = require('./config/config');
 
 let {Message} = require('./models/message');
 
+app.use(bodyParser.json());
+
 mongoose.connect(MONGODB_URI);
 
+// POST route for handling new Message creation
 app.post('/messages', async (req, res) => {
-    console.log('Received POST request to /messages');
-
-    let message = new Message({
-        title: 'Test Message',
-        body: 'Hello World! :D',
-        postedAt: new Date()
-    });
-
     try {
+        let message = new Message({
+            title: req.body.title,
+            body: req.body.body,
+            postedAt: new Date()
+        });
+        
         let doc = await message.save();
         res.send(doc);
     } catch (e) {
+        console.log(e);
         res.status(400).send(e);
     }
 });
