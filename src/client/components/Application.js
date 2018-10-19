5958459322bca68fc1e5e588f29f332821eaf8d3
0Calories <cppanand@gmail.com>
2018-10-19 00:56:30 -0400

    Create Header component and fix factoring

--- src/client/components/Application.js
+++ src/client/components/Application.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './Header';
+
 export default class Application extends React.Component {
 
     render() {
         return (
             <div>
+                <Header />
                 Test
             </div>
         );
